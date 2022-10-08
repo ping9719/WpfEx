dce66a0cf96b7feb290424637d1c014d77269939
ping9719 <971931543@qq.com>
2022-10-08 19:19:57 +0800

    0.0.6

---
+++ Ping9719.WpfEx/Converter/Enum2BoolConverter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace Ping9719.WpfEx
+{
+    /// <summary>
+    /// 将枚举转为布尔
+    /// </summary>
+    [ValueConversion(typeof(Enum), typeof(string))]
+    public class Enum2BoolConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value.ToString() == parameter.ToString();
+        }
+
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return Enum.Parse(targetType, parameter.ToString());
+        }
+    }
+}
