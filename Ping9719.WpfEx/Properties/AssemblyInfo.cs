3ae1da0a9868f48b0c60384b3a0fd4f1edbe600f
ping9719 <971931543@qq.com>
2022-12-06 19:49:49 +0800

    选项卡

--- Ping9719.WpfEx/Properties/AssemblyInfo.cs
+++ Ping9719.WpfEx/Properties/AssemblyInfo.cs
@@ -1,5 +1,7 @@
 ﻿using System.Windows.Markup;
 
+[assembly: System.Windows.ThemeInfo(System.Windows.ResourceDictionaryLocation.None, System.Windows.ResourceDictionaryLocation.SourceAssembly)]
+
 //[assembly: System.Windows.ThemeInfo(System.Windows.ResourceDictionaryLocation.None, System.Windows.ResourceDictionaryLocation.SourceAssembly)]
 [assembly: XmlnsDefinition("https://github.com/ping9719/wpfex", "Ping9719.WpfEx")]
 
