5b86ba089f8800d68534b96eec647015a8c0c006
ping9719 <971931543@qq.com>
2022-12-12 15:57:11 +0800

    RunStateButton

---
+++ Ping9719.WpfEx/Controls/RunStateButton.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Ping9719.WpfEx
+{
+    /// <summary>
+    /// 运行状态
+    /// </summary>
+    public enum RunState
+    {
+        /// <summary>
+        /// 运行中
+        /// </summary>
+        Run,
+        /// <summary>
+        /// 暂停中
+        /// </summary>
+        Pause,
+        /// <summary>
+        /// 已停止
+        /// </summary>
+        Stop,
+
+    }
+
+    /// <summary>
+    /// 运行状态点击的按钮
+    /// </summary>
+    public enum RunStateButtonClick
+    {
+        /// <summary>
+        /// 启动
+        /// </summary>
+        Start,
+        /// <summary>
+        /// 暂停
+        /// </summary>
+        Pause,
+        /// <summary>
+        /// 继续
+        /// </summary>
+        Continue,
+        /// <summary>
+        /// 停止
+        /// </summary>
+        Stop,
+        /// <summary>
+        /// 复位
+        /// </summary>
+        Rset
+    }
+
+    /// <summary>
+    /// 运行状态按钮
+    /// </summary>
+    public class RunStateButton : Control
+    {
+        static RunStateButton()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(RunStateButton), new FrameworkPropertyMetadata(typeof(RunStateButton)));
+        }
+
+        /// <summary>
+        /// 最近一次单击的按钮
+        /// </summary>
+        public RunStateButtonClick RunStateButtonClick { get; set; }
+
+        public override void OnApplyTemplate()
+        {
+            base.OnApplyTemplate();
+
+            var aa1 = this.Template.FindName("_start", this);
+            var aa2 = this.Template.FindName("_pause", this);
+            var aa3 = this.Template.FindName("_continue", this);
+            var aa4 = this.Template.FindName("_stop", this);
+            var aa5 = this.Template.FindName("_rset", this);
+
+            if (aa1 is Button but1)
+                but1.Click += (sen1, obj1) =>
+                {
+                    RunStateButtonClick = RunStateButtonClick.Start;
+                    this.RaiseEvent(new RoutedEventArgs(ClickEvent, RunStateButtonClick));
+                };
+            if (aa2 is Button but2)
+                but2.Click += (sen1, obj1) =>
+                {
+                    RunStateButtonClick = RunStateButtonClick.Pause;
+                    this.RaiseEvent(new RoutedEventArgs(ClickEvent, RunStateButtonClick));
+                };
+            if (aa3 is Button but3)
+                but3.Click += (sen1, obj1) =>
+                {
+                    RunStateButtonClick = RunStateButtonClick.Continue;
+                    this.RaiseEvent(new RoutedEventArgs(ClickEvent, RunStateButtonClick));
+                };
+            if (aa4 is Button but4)
+                but4.Click += (sen1, obj1) =>
+                {
+                    RunStateButtonClick = RunStateButtonClick.Stop;
+                    this.RaiseEvent(new RoutedEventArgs(ClickEvent, RunStateButtonClick));
+                };
+            if (aa5 is Button but5)
+                but5.Click += (sen1, obj1) =>
+                {
+                    RunStateButtonClick = RunStateButtonClick.Rset;
+                    this.RaiseEvent(new RoutedEventArgs(ClickEvent, RunStateButtonClick));
+                };
+        }
+
+        /// <summary>
+        /// 单击按钮时
+        /// </summary>
+        public event RoutedEventHandler Click
+        {
+            add { this.AddHandler(ClickEvent, value); }
+            remove { this.RemoveHandler(ClickEvent, value); }
+        }
+
+        public static readonly RoutedEvent ClickEvent =
+            EventManager.RegisterRoutedEvent("ClickEvent", RoutingStrategy.Bubble, typeof(RoutedEventHandler), typeof(RunStateButton));
+
+        /// <summary>
+        /// 是否显示复位按钮
+        /// </summary>
+        public bool IsVisRset
+        {
+            get { return (bool)GetValue(IsVisRsetProperty); }
+            set { SetValue(IsVisRsetProperty, value); }
+        }
+
+        public static readonly DependencyProperty IsVisRsetProperty =
+            DependencyProperty.Register("IsVisRset", typeof(bool), typeof(RunStateButton), new PropertyMetadata(false));
+
+        /// <summary>
+        /// 运行状态
+        /// </summary>
+        public RunState RunState
+        {
+            get { return (RunState)GetValue(RunStateProperty); }
+            set { SetValue(RunStateProperty, value); }
+        }
+
+        public static readonly DependencyProperty RunStateProperty =
+            DependencyProperty.Register("RunState", typeof(RunState), typeof(RunStateButton), new PropertyMetadata(RunState.Stop));
+
+        public event EventHandler CanExecuteChanged;
+    }
+}
