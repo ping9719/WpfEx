3fde6c3320802f6ad9ccb5730a91d0443eabf08a
Ping9719 <971931543@qq.com>
2023-07-28 17:13:20 +0800

    TextBoxScanner

---
+++ Ping9719.WpfEx/Controls/TextBoxScanner.cs
@@ -0,0 +1,185 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace Ping9719.WpfEx
+{
+    /// <summary>
+    /// 扫码文本框
+    /// </summary>
+    public class TextBoxScanner : TextBox
+    {
+        /// <summary>
+        /// 初始化
+        /// </summary>
+        public TextBoxScanner() : base()
+        {
+            this.IsVisibleChanged += TextBoxScanner_IsVisibleChanged; ;
+            this.TextChanged += TextBoxScanner_TextChanged;
+        }
+
+        int textCount = 0;
+        bool isFir = false;
+        private void TextBoxScanner_IsVisibleChanged(object sender, DependencyPropertyChangedEventArgs e)
+        {
+            if (!isFir)
+            {
+                isFir = true;
+                textCount = Text?.Length ?? 0;
+            }
+
+            if (IsAutoFocus && e.NewValue is true)
+            {
+                Task.Run(() =>
+                {
+                    Thread.Sleep(400);
+                    this.FocusInvoke();
+                });
+            }
+        }
+
+
+        /// <summary>
+        /// 是否自动设置焦点（在显示控件的时候尝试将焦点设置为此元素）
+        /// </summary>
+        public bool IsAutoFocus
+        {
+            get { return (bool)GetValue(IsAutoFocusProperty); }
+            set { SetValue(IsAutoFocusProperty, value); }
+        }
+
+        public static readonly DependencyProperty IsAutoFocusProperty =
+            DependencyProperty.Register("IsAutoFocus", typeof(bool), typeof(TextBoxScanner), new PropertyMetadata(false));
+
+
+        /// <summary>
+        /// 自动清除文本（当达到计时变化间隔时）
+        /// </summary>
+        public bool IsAutoClear
+        {
+            get { return (bool)GetValue(IsAutoClearProperty); }
+            set { SetValue(IsAutoClearProperty, value); }
+        }
+
+        public static readonly DependencyProperty IsAutoClearProperty =
+            DependencyProperty.Register("IsAutoClear", typeof(bool), typeof(TextBoxScanner), new PropertyMetadata(false));
+
+        /// <summary>
+        /// 计时变化间隔，默认600ms
+        /// </summary>
+        public int IntervalTime
+        {
+            get { return (int)GetValue(IntervalTimeProperty); }
+            set { SetValue(IntervalTimeProperty, value); }
+        }
+
+        public static readonly DependencyProperty IntervalTimeProperty =
+            DependencyProperty.Register("IntervalTime", typeof(int), typeof(TextBoxScanner), new PropertyMetadata(600));
+
+        /// <summary>
+        /// 文本扫码改变
+        /// </summary>
+        public event RoutedEventHandler TextScannerChanged
+        {
+            add { this.AddHandler(TextScannerChangedEvent, value); }
+            remove { this.RemoveHandler(TextScannerChangedEvent, value); }
+        }
+
+        public static readonly RoutedEvent TextScannerChangedEvent =
+            EventManager.RegisterRoutedEvent("TextScannerChangedEvent", RoutingStrategy.Bubble, typeof(RoutedEventHandler), typeof(TextBoxScanner));
+
+        /// <summary>
+        /// 扫码文本
+        /// </summary>
+        public string ScannerText { get; set; } = string.Empty;
+
+        /// <summary>
+        /// 尝试将焦点设置为此元素，针对多线程优化
+        /// </summary>
+        public void FocusInvoke()
+        {
+            try
+            {
+                this.Dispatcher.Invoke(() =>
+                {
+                    base.Focus();
+                });
+            }
+            catch (Exception)
+            {
+
+            }
+        }
+
+        DateTime dt = DateTime.Now;
+        bool isRun = false;
+
+        private void TextBoxScanner_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            var textCount_ = Text?.Length ?? 0;
+            //删除文本中
+            if (textCount_ < textCount)
+            {
+                textCount = textCount_;
+                return;
+            }
+
+            textCount = textCount_;
+
+            var isNull = string.IsNullOrEmpty(Text);
+            if (isNull)
+                return;
+
+            dt = DateTime.Now;
+
+            if (isRun)
+            {
+                return;
+            }
+            else
+            {
+                isRun = true;
+                var IntervalTime_ = IntervalTime;
+                Task.Run(() =>
+                {
+                    try
+                    {
+                        while (true)
+                        {
+                            Thread.Sleep(1);
+
+                            //扫码完成
+                            var dtn = DateTime.Now;
+                            if ((dtn - dt).TotalMilliseconds >= IntervalTime_)
+                            {
+                                this.Dispatcher.Invoke(() =>
+                                {
+                                    ScannerText = Text;
+                                    this.RaiseEvent(new RoutedEventArgs(TextScannerChangedEvent));
+
+                                    if (IsAutoClear)
+                                        Text = string.Empty;
+                                });
+                                break;
+                            }
+                        }
+                    }
+                    catch (Exception)
+                    {
+
+                    }
+                    finally
+                    {
+                        isRun = false;
+                    }
+                });
+            }
+        }
+    }
+}
--- WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using Ping9719.WpfEx;
+﻿using HandyControl.Controls;
+using Ping9719.WpfEx;
 using Ping9719.WpfEx.Mvvm;
 using System;
 using System.Collections.Generic;
@@ -23,7 +24,7 @@ namespace WpfApp1
     /// <summary>
     /// MainWindow.xaml 的交互逻辑
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : System.Windows.Window
     {
         MainWindowViewModel ViewModel = new MainWindowViewModel();
 
@@ -36,64 +37,16 @@ namespace WpfApp1
 
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
-            List<DeviceStateData> deviceStateDatas = new List<DeviceStateData>()
-            {
-                new DeviceStateData (){ Name="传感器1",GroupName="",IsOk=false},
-                new DeviceStateData (){ Name="传感器2",GroupName="",IsOk=false},
-                new DeviceStateData (){ Name="传感器3",GroupName="",IsOk=false},
-                new DeviceStateData (){ Name="传感器4",GroupName="1",IsOk=false},
-                new DeviceStateData (){ Name="传感器5",GroupName="1",IsOk=false},
-                new DeviceStateData (){ Name="传感器6",GroupName="1",IsOk=false},
-                new DeviceStateData (){ Name="传感器7",GroupName="1",IsOk=false},
-                new DeviceStateData (){ Name="传感器8",GroupName="2",IsOk=false},
-                new DeviceStateData (){ Name="传感器9",GroupName="2",IsOk=false},
-            };
-            List<DeviceStateSetData> deviceStateSetData = new List<DeviceStateSetData>()
-            {
-                new DeviceStateSetData (){ Name="传感器1",GroupName="",IsOk=false},
-                new DeviceStateSetData (){ Name="传感器2",GroupName="",IsOk=false},
-                new DeviceStateSetData (){ Name="传感器3",GroupName="",IsOk=false},
-                new DeviceStateSetData (){ Name="传感器4",GroupName="1",IsOk=false},
-                new DeviceStateSetData (){ Name="传感器5",GroupName="1",IsOk=false},
-                new DeviceStateSetData (){ Name="传感器6",GroupName="1",IsOk=false},
-                new DeviceStateSetData (){ Name="传感器7",GroupName="1",IsOk=false},
-                new DeviceStateSetData (){ Name="传感器8",GroupName="2",IsOk=false},
-                new DeviceStateSetData (){ Name="传感器9",GroupName="2",IsOk=false},
-            };
-            List<DeviceUrnData> deviceUrnDatas = new List<DeviceUrnData>()
-            {
-                new DeviceUrnData (){Name="气缸1",GroupName="" },
-                new DeviceUrnData (){Name="气缸2",GroupName="" },
-                new DeviceUrnData (){Name="气缸3",GroupName="" },
-                new DeviceUrnData (){Name="气缸4",GroupName="组1" },
-                new DeviceUrnData (){Name="气缸5",GroupName="组1" },
-                new DeviceUrnData (){Name="气缸6",GroupName="组1" },
-                new DeviceUrnData (){Name="气缸7",GroupName="组1" },
-                new DeviceUrnData (){Name="气缸8",GroupName="组2" },
-                new DeviceUrnData (){Name="气缸9",GroupName="组2" },
-            };
-            List<DeviceServo2Data> deviceServoDatas = new List<DeviceServo2Data>()
-            {
-                new DeviceServo2Data (){Name="伺服1",GroupName="" },
-                new DeviceServo2Data (){Name="伺服2",GroupName="" },
-                new DeviceServo2Data (){Name="伺服3",GroupName="" },
-                new DeviceServo2Data (){Name="伺服4",GroupName="组1" },
-                new DeviceServo2Data (){Name="伺服5",GroupName="组1" },
-                new DeviceServo2Data (){Name="伺服6",GroupName="组1" },
-                new DeviceServo2Data (){Name="伺服7",GroupName="组1" },
-                new DeviceServo2Data (){Name="伺服8",GroupName="组2" },
-                new DeviceServo2Data (){Name="伺服9",GroupName="组2" },
-            };
+
         }
 
-        private void clike(object sender, RoutedEventArgs e)
+        private void textc(object sender, RoutedEventArgs e)
         {
-            var aaa = (object[])e.OriginalSource;
-        }
+            var aaa = (TextBoxScanner)sender;
+            var a1 = aaa.Text;
+            var a2 = aaa.ScannerText;
 
-        private void clikeser(object sender, RoutedEventArgs e)
-        {
-            var aaa = (object[])e.OriginalSource;
+            Growl.Info(a1 + ";" + a2);
         }
     }
 
