d4fe1a9037042e105d7352ec9a69c999bfc8accd
Ping9719 <971931543@qq.com>
2024-01-23 17:04:46 +0800

    日志文本框

--- Ping9719.WpfEx/Controls/TextBoxLog.cs
+++ Ping9719.WpfEx/Controls/TextBoxLog.cs
@@ -1,6 +1,10 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices.ComTypes;
 using System.Text;
 using System.Threading;
@@ -20,8 +24,9 @@ namespace Ping9719.WpfEx
         /// <summary>
         /// 是否第一次加载
         /// </summary>
-        public bool IsLoadedFirst { get; private set; } = false;
+        private bool IsLoadedFirst = false;
         private double HeightSum = 0;
+        private bool isColse = false;
 
         /// <summary>
         /// 初始化
@@ -52,28 +57,23 @@ namespace Ping9719.WpfEx
                     }
                 };
 
-                //文本改变置底
-                this.TextChanged += (_, _) =>
-                {
-                    ScrollToEnd2();
-                };
-
-                //关闭窗体释放
+                //加载控件
                 this.Loaded += (_, _) =>
                 {
                     if (AutoScrollToEnd)
-                    {
                         ScrollToEnd();
-                    }
 
                     if (!IsLoadedFirst)
                     {
+                        //所在窗体
                         try
                         {
                             Window parentWindow = Window.GetWindow(this);
                             if (parentWindow != null)
                                 parentWindow.Closed += (_, _) =>
                                 {
+                                    isColse = true;
+                                    base.Resources.Clear();
                                     TextBoxLogAll.Remove(this);
                                 };
                         }
@@ -81,14 +81,85 @@ namespace Ping9719.WpfEx
                         {
 
                         }
+
+                        //刷新
+                        Task.Run(async () =>
+                        {
+                            while (!isColse)
+                            {
+                                await Task.Delay(10);
+
+                                var dt = DateTime.Now;
+                                while (!queue.IsEmpty)
+                                {
+                                    if (stringb.Length > 0)
+                                        stringb.Append(Environment.NewLine);
+
+                                    if (queue.TryDequeue(out string a))
+                                        stringb.Append(a);
+
+                                    if (queue.IsEmpty || (DateTime.Now - dt).TotalMilliseconds > 20)
+                                    {
+                                        this?.Dispatcher?.Invoke(() =>
+                                        {
+                                            BeginChange();
+
+                                            if (AddTextClearSelection)
+                                                SelectionLength = 0;
+                                            var onestr = (string.IsNullOrEmpty(Text) ? string.Empty : Environment.NewLine);
+                                            bool isScroll = AutoScrollToEnd && (LineCount - GetLastVisibleLineIndex()) < 5;
+                                            AppendText(onestr);
+                                            AppendText(stringb.ToString());
+                                            stringb.Clear();
+
+                                            //清理
+                                            if (LineCount >= MaxLineNumVal)
+                                            {
+                                                if (MaxLineNumClearAll)
+                                                {
+                                                    base.Clear();
+                                                }
+                                                else
+                                                {
+                                                    try
+                                                    {
+                                                        var num = GetCharacterIndexFromLineIndex(MaxLineNum / 4);
+                                                        if (num > 0 && num < Text.Length)
+                                                            Text = Text.Substring(num);
+                                                    }
+                                                    catch (Exception)
+                                                    {
+
+                                                    }
+                                                }
+                                            }
+
+                                            if (isScroll)
+                                                ScrollToEnd();
+
+                                            EndChange();
+                                        });
+
+                                        break;
+                                    }
+                                }
+                            }
+                        });
                     }
+
                     IsLoadedFirst = true;
                 };
             }
         }
 
+
         /// <summary>
-        /// 格式时间字符串。默认[HH:mm:ss.fff]
+        /// 最大行数
+        /// </summary>
+        public string TimeFormatVal { get; private set; } = "HH:mm:ss.fff=>";
+
+        /// <summary>
+        /// 格式时间字符串。默认 HH:mm:ss.fff=>
         /// </summary>
         public string TimeFormat
         {
@@ -97,10 +168,25 @@ namespace Ping9719.WpfEx
         }
 
         public static readonly DependencyProperty TimeFormatProperty =
-            DependencyProperty.Register("TimeFormat", typeof(string), typeof(TextBoxLog), new PropertyMetadata("[HH:mm:ss.fff]"));
+            DependencyProperty.Register("TimeFormat", typeof(string), typeof(TextBoxLog), new PropertyMetadata("HH:mm:ss.fff=>", OnTimeFormatChanged));
+
+        private static void OnTimeFormatChanged(DependencyObject o, DependencyPropertyChangedEventArgs e)
+        {
+            if (o == null || e == null)
+                return;
+            if (e.NewValue is string val && o is TextBoxLog tbl)
+            {
+                tbl.TimeFormatVal = val;
+            }
+        }
 
         /// <summary>
-        /// 清除时保留的最大行的数量（数量的1.5倍触发截断）。默认600
+        /// 最大行数
+        /// </summary>
+        public int MaxLineNumVal { get; private set; } = 400;
+
+        /// <summary>
+        /// 最大行数（超过自动清除4分之1）。默认400
         /// </summary>
         public int MaxLineNum
         {
@@ -109,7 +195,17 @@ namespace Ping9719.WpfEx
         }
 
         public static readonly DependencyProperty MaxLineNumProperty =
-            DependencyProperty.Register("MaxLineNum", typeof(int), typeof(TextBoxLog), new PropertyMetadata(600));
+            DependencyProperty.Register("MaxLineNum", typeof(int), typeof(TextBoxLog), new PropertyMetadata(400, OnMaxLineNumChanged));
+
+        private static void OnMaxLineNumChanged(DependencyObject o, DependencyPropertyChangedEventArgs e)
+        {
+            if (o == null || e == null)
+                return;
+            if (e.NewValue is int val && o is TextBoxLog tbl)
+            {
+                tbl.MaxLineNumVal = val;
+            }
+        }
 
         /// <summary>
         /// 满足了清除条件时，是否清空所有文本。默认false
@@ -135,6 +231,18 @@ namespace Ping9719.WpfEx
 
         public static readonly DependencyProperty AutoScrollToEndProperty =
             DependencyProperty.Register("AutoScrollToEnd", typeof(bool), typeof(TextBoxLog), new PropertyMetadata(true));
+
+        /// <summary>
+        /// 添加文本时，清空用户的选择。对性能有影响提升。（默认false）
+        /// </summary>
+        public bool AddTextClearSelection
+        {
+            get { return (bool)GetValue(AddTextClearSelectionProperty); }
+            set { SetValue(AddTextClearSelectionProperty, value); }
+        }
+
+        public static readonly DependencyProperty AddTextClearSelectionProperty =
+            DependencyProperty.Register("AddTextClearSelection", typeof(bool), typeof(TextBoxLog), new PropertyMetadata(false));
 
 
         /// <summary>
@@ -164,14 +272,24 @@ namespace Ping9719.WpfEx
             }
         }
 
+        ConcurrentQueue<string> queue = new ConcurrentQueue<string>();
+        StringBuilder stringb = new StringBuilder();
+        /// <summary>
+        /// 将字符串追加到文本
+        /// </summary>
+        /// <param name="text"></param>
+        public void AppendText2(string text)
+        {
+            if (queue.Count > MaxLineNumVal)
+                queue.TryDequeue(out string que);
+
+            queue.Enqueue(text);
+        }
+
         private void ScrollToEnd2()
         {
-            if (AutoScrollToEnd)
-            {
-                var cha = LineCount - GetLastVisibleLineIndex();
-                if (cha < 5)
-                    ScrollToEnd();
-            }
+            if (AutoScrollToEnd && (LineCount - GetLastVisibleLineIndex()) < 5)
+                ScrollToEnd();
         }
 
         #region 静态
@@ -187,6 +305,10 @@ namespace Ping9719.WpfEx
         /// 所有的正在使用的日志组件
         /// </summary>
         private static List<TextBoxLog> TextBoxLogAll = new List<TextBoxLog>();
+        /// <summary>
+        /// 组件数量
+        /// </summary>
+        public static int TextBoxLogCount { get => TextBoxLogAll.Count; }
 
         /// <summary>
         /// 追加日志信息
@@ -199,90 +321,51 @@ namespace Ping9719.WpfEx
         /// <summary>
         /// 追加日志信息
         /// </summary>
-        public static void AddLog(string text, string token)
-        {
-            AddLog(text, token, null, null);
-        }
-
-        /// <summary>
-        /// 追加日志信息
-        /// </summary>
         public static void AddLog(string text, string token = "", DateTime? time = null, object tag = null)
         {
-            TextBoxLog tbl = GetTextBoxLog(token);
-            TextBoxLogInfo info = new TextBoxLogInfo()
-            {
-                Token = token,
-                IsSeekToken = tbl != null,
-                Time = time.HasValue ? time.Value : DateTime.Now,
-                Text = text,
-                Tag = tag,
-            };
+            AddLogs(new string[] { text }, token, time, tag);
+        }
 
-            if (tbl == null)
-            {
-                TextBoxLogAdd?.BeginInvoke(info, null, null);
+        /// <summary>
+        /// 追加多条日志信息
+        /// </summary>
+        public static void AddLogs(IEnumerable<string> texts, string token = "", DateTime? time = null, object tag = null)
+        {
+            TextBoxLog tbl = SeeControl(token);
+
+            if (texts == null)
                 return;
-            }
 
-            try
+            foreach (var text in texts)
             {
-                tbl?.Dispatcher?.Invoke(() =>
+                TextBoxLogInfo info = new TextBoxLogInfo()
                 {
-                    info.SeekToken = tbl.TokenVal;
-                    info.TimeFormat = tbl.TimeFormat;
-                    tbl.BeginChange();
-                    tbl.AppendText($"{(string.IsNullOrEmpty(tbl.Text) ? "" : Environment.NewLine)}{info.ToString()}");
+                    Text = text,
+                    Time = time.HasValue ? time.Value : DateTime.Now,
+                    Tag = tag,
+                    Token = token,
+                    IsSeekToken = tbl != null,
+                    SeekToken = tbl?.TokenVal,
+                    TimeFormat = tbl?.TimeFormatVal,
+                };
 
-                    //清理
-                    if (tbl.LineCount > tbl.MaxLineNum * 1.5)
-                    {
-                        if (tbl.MaxLineNumClearAll)
-                        {
-                            ClearLog(tbl);
-                        }
-                        else
-                        {
-                            try
-                            {
-                                var num = tbl.GetCharacterIndexFromLineIndex(tbl.MaxLineNum);
-                                tbl.Text = tbl.Text.Substring(num);
-                            }
-                            catch (Exception ex)
-                            {
-                                ClearLog(tbl);
-
-                                info.AddErr = ex;
-                                TextBoxLogAdd?.BeginInvoke(info, null, null);
-                                tbl.EndChange();
-                                return;
-                            }
-                        }
-                    }
-                    tbl.EndChange();
-
-                    TextBoxLogAdd?.BeginInvoke(info, null, null);
-                });
-            }
-            catch (Exception ex)
-            {
-                info.AddErr = ex;
+                tbl?.AppendText2(info.ToString());
                 TextBoxLogAdd?.BeginInvoke(info, null, null);
             }
         }
 
         /// <summary>
-        /// 清除日志
+        /// 清除所有信息
         /// </summary>
         /// <param name="token"></param>
         public static void ClearLog(string token = "")
         {
-            TextBoxLog tbl = GetTextBoxLog(token);
+            TextBoxLog tbl = SeeControl(token);
             ClearLog(tbl);
         }
 
         /// <summary>
-        /// 清除日志
+        /// 清除所有信息
         /// </summary>
         /// <param name="tbl"></param>
         public static void ClearLog(TextBoxLog tbl)
@@ -305,7 +388,7 @@ namespace Ping9719.WpfEx
         /// </summary>
         /// <param name="token"></param>
         /// <returns></returns>
-        public static TextBoxLog GetTextBoxLog(string token)
+        public static TextBoxLog SeeControl(string token)
         {
             foreach (var item in TextBoxLogAll)
             {
@@ -366,10 +449,6 @@ namespace Ping9719.WpfEx
         /// 找寻到的组件的时间格式字符串
         /// </summary>
         public string TimeFormat { get; set; }
-        /// <summary>
-        /// 添加到页面时的错误
-        /// </summary>
-        public Exception AddErr { get; set; }
 
         /// <summary>
         /// 转为字符串
