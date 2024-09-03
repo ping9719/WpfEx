a2f36810417c0059b10a487eeec4676e1af3e8d2
Ping9719 <971931543@qq.com>
2024-09-03 16:01:02 +0800

    mainload

--- Ping9719.WpfEx/Windows/MainLoadWindow.xaml.cs
+++ Ping9719.WpfEx/Windows/MainLoadWindow.xaml.cs
@@ -14,6 +14,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using MessageBox = HandyControl.Controls.MessageBox;
 
 namespace Ping9719.WpfEx
 {
@@ -23,13 +24,13 @@ namespace Ping9719.WpfEx
     public partial class MainLoadWindow : HandyControl.Controls.Window
     {
         /// <summary>
-        /// 任务最小间隔时间
+        /// 任务最短停留时间，默认180ms（视觉暂留0.1，人眼辨认0.3）
         /// </summary>
-        public static int TaskSleepTime = 300;
+        public static int TaskSleepTime = 180;
         Tuple<string, string, Action>[] Funcs;
         MainLoadWindowViewModel vModel = new MainLoadWindowViewModel();
-        bool isUserClose = false;
-        string err = "";
+        bool isErr = false;
+        string errText = "";
 
         internal MainLoadWindow()
         {
@@ -37,67 +38,65 @@ namespace Ping9719.WpfEx
             this.DataContext = vModel;
         }
 
-        private void Window_Loaded(object sender, RoutedEventArgs e)
+        private async void Window_Loaded(object sender, RoutedEventArgs e)
         {
-            Task.Run(() =>
+            await Task.Run(async () =>
             {
-                if (Funcs == null)
-                    return;
-
-                Stopwatch stopwatch = new Stopwatch();
                 foreach (var item in Funcs)
                 {
                     vModel.Txt = item.Item1;
                     try
                     {
-                        stopwatch.Restart();
+                        var dt1 = DateTime.Now;
                         item.Item3.Invoke();
-                        stopwatch.Stop();
-                        if (stopwatch.ElapsedMilliseconds < TaskSleepTime)
-                        {
-                            Thread.Sleep(TaskSleepTime - Convert.ToInt32(stopwatch.ElapsedMilliseconds));
-                        }
+                        var dt2 = (int)(DateTime.Now - dt1).TotalMilliseconds;
+
+                        if (dt2 < TaskSleepTime)
+                            await Task.Delay(TaskSleepTime - dt2);
                     }
                     catch (Exception ex)
                     {
+                        isErr = true;
                         vModel.Txt = item.Item2;
                         vModel.IsClose = true;
                         vModel.IsVisLoad = false;
-                        err = ex.ToString();
-                        stopwatch.Stop();
-                        return;
+                        errText = ex.ToString();
+                        break;
                     }
                 }
+            });
 
+            if (!isErr)
                 this.Dispatcher.Invoke(() =>
                 {
                     this.Close();
                 });
-            });
         }
 
         /// <summary>
-        /// 开始显示
+        /// 显示并运行
         /// </summary>
         /// <param name="funcs">任务，1正在进行的任务的提示（如：正在进行XX中...），2此任务错误的提示（如：XX失败），3任务</param>
         /// <returns>是否全部成功</returns>
         public static bool Show(params Tuple<string, string, Action>[] funcs)
         {
+            if (funcs == null || !funcs.Any())
+                return true;
+
             MainLoadWindow mainLoadWindow = new MainLoadWindow();
             mainLoadWindow.Funcs = funcs;
             mainLoadWindow.ShowDialog();
-            return !mainLoadWindow.isUserClose;
+            return !mainLoadWindow.isErr;
         }
 
         private void TextBlock_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
         {
-            if (!string.IsNullOrWhiteSpace(err))
-                MessageBox.Show(err);
+            if (!string.IsNullOrWhiteSpace(errText))
+                MessageBox.Show(errText);
         }
 
         private void but_close(object sender, RoutedEventArgs e)
         {
-            isUserClose = true;
             this.Close();
         }
 
@@ -110,14 +109,12 @@ namespace Ping9719.WpfEx
     internal class MainLoadWindowViewModel : BindableBase
     {
         private string txt;
-
         public string Txt { get => txt; set { SetProperty(ref txt, value); } }
 
         private bool isVisLoad = true;
         public bool IsVisLoad { get => isVisLoad; set { SetProperty(ref isVisLoad, value); } }
 
         private bool isclode = false;
-
         public bool IsClose { get => isclode; set { SetProperty(ref isclode, value); } }
     }
 }
